3baf9f00b952a3da466950913cf7ddb84fa3bd1f
Daniel Batista <oliveiradb1@gmail.com>
2022-10-28 13:11:39 -0300

    recommended update

--- src/components/pages/product/Produtos.jsx
+++ src/components/pages/product/Produtos.jsx
@@ -3,9 +3,9 @@ import "./Produtos.css";
 import content from "../../../content";
 import contentNovidades from "../../Novidades/contentNovidades";
 import { useStateContext } from "../../../context/StateContext";
-import Cabide from '../../../img/iconsProvador/Cabide.svg'
-import Metrica from '../../../img/iconsProvador/Metrica.svg'
-import {Products} from '../home/products'
+import Cabide from "../../../img/iconsProvador/Cabide.svg";
+import Metrica from "../../../img/iconsProvador/Metrica.svg";
+import { Recommended } from "./recommended";
 
 export const Produtos = () => {
   const {
@@ -28,145 +28,163 @@ export const Produtos = () => {
 
   return (
     <>
-    <div className="navbar-container1">
-      {idNovo.map((item) => (
-        <div key={item.key} className="product-detail-container1">
-          <div className="image-container1">
-            <img src={item.imagem} className="product-detail-image1" alt="" />
-          </div>
-          <div>
-            <div className="product-detail-desc1">
-              <span className="product-name">{item.name}</span>
-              <div className="reviews1">
-                Vendido e entregue por
-                <span className="destaque"> GOFASHION</span>
-              </div>
-              <div>
-                <div className="select-color">
-                  Selecionar Cor:{" "}
-                  <span className="select-color-name">{item.color}</span>
+      <div className="navbar-container1">
+        {idNovo.map((item) => (
+          <div key={item.key} className="product-detail-container1">
+            <div className="image-container1">
+              <img src={item.imagem} className="product-detail-image1" alt="" />
+            </div>
+            <div>
+              <div className="product-detail-desc1">
+                <span className="product-name">{item.name}</span>
+                <div className="reviews1">
+                  Vendido e entregue por
+                  <span className="destaque"> GOFASHION</span>
                 </div>
-                <div
-                  className="color-tshirt-container"
-                  style={{ border: `2px solid #${item.colorHex}` }}
-                >
+                <div>
+                  <div className="select-color">
+                    Selecionar Cor:{" "}
+                    <span className="select-color-name">{item.color}</span>
+                  </div>
                   <div
-                    className="color-tshirt-container1"
-                    style={{ background: `#${item.colorHex}` }}
-                  ></div>
-                  
+                    className="color-tshirt-container"
+                    style={{ border: `2px solid #${item.colorHex}` }}
+                  >
+                    <div
+                      className="color-tshirt-container1"
+                      style={{ background: `#${item.colorHex}` }}
+                    ></div>
+                  </div>
                 </div>
-              </div>
-              <div className="container-size">
-                <div className="product-descricao-tamanho">
-                  Selecionar Tamanho: {selected}
+                <div className="container-size">
+                  <div className="product-descricao-tamanho">
+                    Selecionar Tamanho: {selected}
+                  </div>
+                  <form className="buttons-size">
+                    <input
+                      type="radio"
+                      id="PP"
+                      name="size"
+                      value="PP"
+                      onChange={handleChange}
+                      className="input-size"
+                    />
+                    <label className="label-size" for="PP">
+                      PP
+                    </label>
+
+                    <input
+                      type="radio"
+                      id="P"
+                      name="size"
+                      value="P"
+                      onChange={handleChange}
+                      className="input-size"
+                    />
+                    <label className="label-size" for="P">
+                      P
+                    </label>
+
+                    <input
+                      type="radio"
+                      id="M"
+                      name="size"
+                      value="M"
+                      onChange={handleChange}
+                      className="input-size"
+                    />
+                    <label className="label-size" for="M">
+                      M
+                    </label>
+
+                    <input
+                      type="radio"
+                      id="G"
+                      name="size"
+                      value="G"
+                      onChange={handleChange}
+                      className="input-size"
+                    />
+                    <label className="label-size" for="G">
+                      G
+                    </label>
+                  </form>
                 </div>
-                <form className="buttons-size">              
-                  <input
-                    type="radio"
-                    id="PP"
-                    name="size"
-                    value="PP"
-                    onChange={handleChange}
-                    className = 'input-size'
-                  />
-                  <label className = 'label-size' for="PP">PP</label>
 
-                  <input
-                    type="radio"
-                    id="P"
-                    name="size"
-                    value="P"
-                    onChange={handleChange}
-                    className = 'input-size'
-                  />
-                  <label className = 'label-size' for="P">P</label>
+                <div className="vestuario">
+                  <div>
+                    <img className="cabide" src={Cabide} alt="" title="" />
+                    <span className="cabide-text">Provador Virtual</span>
+                  </div>
+                  <div>
+                    <img className="metrica" src={Metrica} alt="" title="" />
+                    <span className="metrica-text">Tabela de Medidas</span>
+                  </div>
+                </div>
 
-                  <input
-                    type="radio"
-                    id="M"
-                    name="size"
-                    value="M"
-                    onChange={handleChange}
-                      className = 'input-size'
-                  />
-                  <label className = 'label-size' for="M">M</label>
-
-                  <input
-                    type="radio"
-                    id="G"
-                    name="size"
-                    value="G"
-                    onChange={handleChange}
-                    className = 'input-size'
-                  />
-                  <label className = 'label-size' for="G">G</label>
-
-                </form>
-              </div>
-
-              <div className="vestuario">
-                <div><img className="cabide" src={Cabide} alt="" title=""/><span className="cabide-text">Provador Virtual</span></div>
-                <div><img className="metrica" src={Metrica} alt="" title=""/><span className="metrica-text">Tabela de Medidas</span></div>
-              </div>
-
-              <div className="productList1">
-                <a href=" " className="preco1">
-                  {item.valorDesconto === "" ? "" : `R$${item.valorDesconto}0`}
-                </a>
-                {item.promo === "" ? " " : `(${item.promo}% off)`}
-              </div>
-
-              <p className="price1">R$ {item.price}0</p>
-              <p>
-                Em até 3x {(item.price / 3).toFixed(2)} sem juros{" "}
-                <span className="destaque">GOFASHION</span>
-              </p>
-
-              <div className="buttons1">
-                <button
-                  type="button"
-                  className="add-to-cart1"
-                  onClick={selected ? () => onAdd(item, qty) : `Por favor, escolha uma opção`}                  
-                >
-                  ADICIONAR À SACOLA
-                </button>
-                {console.log(selected+'oi')}
-                {cartItems.length > 0 && (
-                  <a href="/checkout">
-                    <button type="button" className="buy-now1">
-                      COMPRAR COM 1 CLIQUE
-                    </button>
+                <div className="productList1">
+                  <a href=" " className="preco1">
+                    {item.valorDesconto === ""
+                      ? ""
+                      : `R$${item.valorDesconto}0`}
                   </a>
-                )}
+                  {item.promo === "" ? " " : `(${item.promo}% off)`}
+                </div>
+
+                <p className="price1">R$ {item.price}0</p>
+                <p>
+                  Em até 3x {(item.price / 3).toFixed(2)} sem juros{" "}
+                  <span className="destaque">GOFASHION</span>
+                </p>
+
+                <div className="buttons1">
+                  <button
+                    type="button"
+                    className="add-to-cart1"
+                    onClick={
+                      selected
+                        ? () => onAdd(item, qty)
+                        : `Por favor, escolha uma opção`
+                    }
+                  >
+                    ADICIONAR À SACOLA
+                  </button>
+                  {console.log(selected + "oi")}
+                  {cartItems.length > 0 && (
+                    <a href="/checkout">
+                      <button type="button" className="buy-now1">
+                        COMPRAR COM 1 CLIQUE
+                      </button>
+                    </a>
+                  )}
+                </div>
               </div>
             </div>
           </div>
+        ))}
+        <div className="rec">
+          <div className="title-rec"><b>Outros clientes estão comprando:</b></div>
+          {content
+            .filter((item) => item.categoria === "Vestuário C")
+            .map((produto, i) => (
+              <Recommended
+                imagem={produto.imagem}
+                name={produto.name}
+                price={produto.price}
+                totalVendindos={produto.totalVendindos}
+                fimPromocao={produto.fimPromocao}
+                rating={produto.rating}
+                promo={produto.promo}
+                valorDesconto={produto.valorDesconto}
+                color={produto.color}
+                colorHex={produto.colorHex}
+                todosProdutos={produto}
+                id={produto.id}
+                key={i}
+              />
+            ))}
         </div>
-      ))}
-      <div className="Rec">
-    
-      {content.filter((item) => item.categoria === "Vestuário C").map((produto,i) => (
-           
-          <Products
-            imagem={produto.imagem}
-            name={produto.name}
-            price={produto.price}
-            totalVendindos={produto.totalVendindos}
-            fimPromocao={produto.fimPromocao}
-            rating={produto.rating}
-            promo={produto.promo}
-            valorDesconto={produto.valorDesconto}
-            color={produto.color}
-            colorHex={produto.colorHex}
-            todosProdutos={produto}
-            id={produto.id}
-            key={i}
-          />
-      ))}
-    </div>
-    </div>
-
+      </div>
     </>
   );
 };
--- src/components/pages/product/recommended.jsx
+++ src/components/pages/product/recommended.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { FaShoppingCart, FaRegBookmark, FaStar } from "react-icons/fa";
 
-const Recommended = () => {
-    return ( <p></p> );
+import { useStateContext } from "../../../context/StateContext";
+
+export function Recommended(prod) {
+  const { qty, onAdd, screenProduct } = useStateContext();
+
+  return (
+    <div className="productList">
+      <div key={prod.id} className="productCard">
+        <div className="descount2">
+          {prod.promo === "" ? (
+            " "
+          ) : (
+            <a className="descount" href="/#">
+              {prod.promo}% off
+            </a>
+          )}
+        </div>
+        <a href={`/product/${prod.name}`} onClick={() => screenProduct(prod.todosProdutos)}><img src={prod.imagem} alt="product-img" className="productImage"></img></a>
+
+        <a href="/#" onClick={() => onAdd(prod.todosProdutos, qty)}>
+          <FaShoppingCart
+            className="productCard__cart"
+            title="Adicionar Carrinho"
+          />
+        </a>
+
+        <FaRegBookmark className="productCard__wishlist" title="Favoritar" />
+
+        <div className="productCard__content">
+          <h3 className="productName">{prod.name}</h3>
+          <div className="displayStack__1">
+            <div className="productPrice">
+              <a href="/#" className="preco">
+                {prod.valorDesconto === "" ? "" : `R$${prod.valorDesconto}0`}
+              </a>
+              R${ prod.price}0
+            </div>
+            <div className="productSales">{prod.totalVendindos} unidades</div>
+          </div>
+
+          <div className="displayStack__2">
+            <div className="productRating">
+              {[...Array(prod.rating)].map((item, i) => (
+                <FaStar id={item + 1} key={i} />
+              ))}
+            </div>
+            <div className="productTime">
+              {prod.fimPromocao} Dias Para o Fim da Promoção
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
- 
-export default Recommended;
